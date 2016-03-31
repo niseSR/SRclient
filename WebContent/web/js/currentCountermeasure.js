8cc7f7aa2215c8d643ac2cf5b1276b0ec7225634
Bongjae_Kim <drzakalkpj@gmail.com>
2016-03-31 18:51:35 +0900

    봉재 커밋 : Architecture Info 부분 완료   - 해야할 것 데이터 불러오는 것 및 데이터 삭제를 위한 로직 그리고 Risk Assessment 시작

---
+++ WebContent/web/js/currentCountermeasure.js
@@ -0,0 +1,150 @@
+var domainText = getDomain();
+
+function findAssetType()
+{
+	var param = "assetType=AS_" + $("#AssetType1 option:selected").val();
+	
+	if ($("#AssetType1 option:selected").val() =="0" )
+	{
+		alert("Please select the asset category. \n Asset의 대분류를 반드시 선택해 주세요.");
+		return false;
+	}
+	
+	$.ajax({
+        type: "POST",
+        url: "http://" + domainText + "/SRserver/arcinfo/select_AssetType.do",
+        callback:"callbak",
+		dataType: "jsonp",
+		data:param,	
+		success:
+			function(data){
+        	$.each(data, function(k,v){
+            	if(k=="success"){
+            		var html = '';
+					$.each(v, function(l,m){
+                  		html += '<option value="' + m["assetID"] + '">'+m["assetName"]+'</option>';
+                	});
+            		$("#AssetType2").html(html);
+            		alert("Success to get the data. \n 성공적으로 가져왔습니다. ");
+            	}
+            	if(k=="fail"){
+            		alert("Fail to get the data. \n 정보를 가져오는데에 실패하였습니다.");
+            	}
+        	});
+        },
+        error: function(){
+        	alert("Fail to access the server \n 서버 연결 실패");
+        	return false;
+        }
+    });
+    return true;
+}
+
+function findCountermeasureType()
+{
+	
+	if ($("CountermeasureType1 option:selected").val() =="0" )
+	{
+		alert("Please select the countermeasure category. \n countermeasure의 대분류를 반드시 선택해 주세요.");
+		return false;
+	}
+	
+	var resultAssetValue = $("#AssetType2 option:selected").val().replace("AS_", "");
+	var param = "cmType=C_" + resultAssetValue + "_" + $("#CountermeasureType1 option:selected").val() + "_";
+
+	$.ajax({
+        type: "POST",
+        url: "http://" + domainText + "/SRserver/arcinfo/select_CountermeasureType.do",
+        callback:"callbak",
+		dataType: "jsonp",
+		data:param,	
+		success:
+			function(data){
+        	$.each(data, function(k,v){
+            	if(k=="success"){
+            		var html = '';
+					$.each(v, function(l,m){
+                  		html += '<option value="' + m["cmID"] + '">'+m["cmName"]+'</option>';
+                	});
+            		$("#CountermeasureType2").html(html);
+            		alert("Success to get the data. \n 성공적으로 가져왔습니다. ");
+            	}
+            	if(k=="fail"){
+            		alert("Fail to get the data. \n 정보를 가져오는데에 실패하였습니다.");
+            	}
+        	});
+        },
+        error: function(){
+        	alert("Fail to access the server \n 서버 연결 실패");
+        	return false;
+        }
+    });
+    return true;
+}
+
+function findImplementableCM()
+{
+	var param = "implcmType=" + $("#CountermeasureType2 option:selected").val() + "_";
+
+	$.ajax({
+        type: "POST",
+        url: "http://" + domainText + "/SRserver/arcinfo/select_ImplCountermeasureType.do",
+        callback:"callbak",
+		dataType: "jsonp",
+		data:param,	
+		success:
+			function(data){
+        	$.each(data, function(k,v){
+            	if(k=="success"){
+            		var html = '';
+					$.each(v, function(l,m){
+                  		html += '<option value="' + m["implcmID"] + '">'+m["implcmName"]+'</option>';
+                	});
+            		$("#ImplCountermeasure").html(html);
+            		alert("Success to get the data. \n 성공적으로 가져왔습니다. ");
+            	}
+            	if(k=="fail"){
+            		alert("Fail to get the data. \n 정보를 가져오는데에 실패하였습니다.");
+            	}
+        	});
+        },
+        error: function(){
+        	alert("Fail to access the server \n 서버 연결 실패");
+        	return false;
+        }
+    });
+    return true;
+}
+
+function submitCurrentCMInfo()
+{
+	var param = "currentcmCMID=" + $("#CountermeasureType2 option:selected").val() 
+		+ "&currentcmImplcmID=" + $("#ImplCountermeasure option:selected").val()
+		+ "&currentcmUserID=" + implement_pbkdf2(sessionStorage.getItem("SRClient.id"));
+
+	$.ajax({
+        type: "POST",
+        url: "http://" + domainText + "/SRserver/arcinfo/submit_CurrentCMInfo.do",
+        callback:"callbak",
+		dataType: "jsonp",
+		data:param,	
+		success:
+			function(data){
+        	$.each(data, function(k,v){
+            	if(k=="success"){
+            		alert("Success to register the data. \n 성공적으로 등록하였습니다. ");
+            		location.href = './currentCountermeasure.html'
+            	}
+            	if(k=="fail"){
+            		alert("Fail to register the data. \n 정보를 가져오는데에 실패하였습니다.");
+            	}
+        	});
+        },
+        error: function(){
+        	alert("Fail to access the server \n 서버 연결 실패");
+        	return false;
+        }
+    });
+    return true;
+}
+
